0108d9792e7d7f7b84109617e073e6b1d807fc13
HASSANI Fatima Zahra <hassani.fatimazahera199@gmail.com>
2022-03-27 20:53:23 +0000

    add JS Tools

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
+import ReactDOM from "react-dom";
+import React from "react";
+import Pet from "./Pet";
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
