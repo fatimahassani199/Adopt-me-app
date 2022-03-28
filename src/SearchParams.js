fdfd4aee02ff57bf2ab5dc8edc21621b2ae9d7d6
Fatima Zahra Hassani <fzhassani@SMB.SQLI.COM>
2022-03-28 12:32:05 +0000

    Special Case React Tools

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  //states
-  const [location, updateLocation] = useState("Seattle, WA");
   const [animal, updateAnimal] = useState("");
+  const [location, updateLocation] = useState("");
   const [breed, updateBreed] = useState("");
-  const [breeds] = useBreedList(animal);
   const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
     const res = await fetch(
@@ -74,9 +76,22 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
-      <Results pets={pets} />;
+      <Results pets={pets} />
     </div>
   );
 };
